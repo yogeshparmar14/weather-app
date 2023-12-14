fa3d583b5d9f2f53ceee934fd4d216a2c1f9fe4e
yogeshparmar14 <mr.yoraj@gmail.com>
2023-12-14 13:18:16 +0530

    intail commit

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -47,7 +47,7 @@ export default function HomeScreen() {
 
   const fetchMyWeatherData = async ()=>{
     let myCity = await getData('city');
-    let cityName = 'Islamabad';
+    let cityName = 'New Delhi';
     if(myCity){
       cityName = myCity;
     }
